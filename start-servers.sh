e1240874be19c466194a458946e211072acb242f
Don Morehouse <dm.wyncode@gmail.com>
2020-12-30 20:40:41 +0000

    Change java jar name to reflect updated version. 	modified:   start-servers.sh

--- start-servers.sh
+++ start-servers.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 TOOLS=$(pwd)/tools
+VERSION='3.141.59'
 cd $TOOLS/browsermob-proxy-2.1.4/bin/ && \
 ./browsermob-proxy --port 9090 & \
 cd $TOOLS && \
-java -Dwebdriver.chrome.driver=chromedriver -Dwebdriver.gecko.driver=geckodriver -jar selenium-server-standalone-3.0.1.jar &
+java -Dwebdriver.chrome.driver=chromedriver -Dwebdriver.gecko.driver=geckodriver -jar selenium-server-standalone-$VERSION.jar &
