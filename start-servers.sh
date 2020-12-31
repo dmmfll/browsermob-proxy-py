9efe48228e811368b383580d171d5638eba5d035
Don Morehouse <dm.wyncode@gmail.com>
2020-12-31 16:28:11 +0000

    Properly start the stand-alone server. 	modified:   start-servers.sh

--- start-servers.sh
+++ start-servers.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 TOOLS=$(pwd)/tools
 VERSION='3.141.59'
+JAR=selenium-server-standalone-$VERSION.jar
+NETLOC='172.17.0.1'
 cd $TOOLS/browsermob-proxy-2.1.4/bin/ && \
 ./browsermob-proxy --port 9090 & \
 cd $TOOLS && \
-java -Dwebdriver.chrome.driver=chromedriver -Dwebdriver.gecko.driver=geckodriver -jar selenium-server-standalone-$VERSION.jar &
+java -jar $JAR -role hub &
+cd $TOOLS && \
+java -Dwebdriver.chrome.driver=$(which chromedriver) -Dwebdriver.gecko.driver=$(which geckodriver) -jar $JAR -role node -hub http://$NETLOC:4444/grid/register/ &
+# See https://stackoverflow.com/a/52829112/1913726
+# java -Dwebdriver.chrome.driver=/path/to/chromedriver.exe -jar /Users/admin/selenium-server-standalone-3.14.0.jar -role node -hub http://<IP_GRID_HUB>:4444/grid/register
