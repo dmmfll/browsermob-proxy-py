90a69c34777cff02fb06ab3a97085124f82d48c7
Don Morehouse <dm.wyncode@gmail.com>
2020-12-31 19:11:02 +0000

    Add logs. 	modified:   start-servers.sh

--- start-servers.sh
+++ start-servers.sh
@@ -6,8 +6,8 @@ NETLOC='172.17.0.1'
 cd $TOOLS/browsermob-proxy-2.1.4/bin/ && \
 ./browsermob-proxy --port 9090 & \
 cd $TOOLS && \
-java -jar $JAR -role hub &
+java -jar $JAR -role hub -log log.txt &
 cd $TOOLS && \
-java -Dwebdriver.chrome.driver=$(which chromedriver) -Dwebdriver.gecko.driver=$(which geckodriver) -jar $JAR -role node -hub http://$NETLOC:4444/grid/register/ &
+java -Dwebdriver.chrome.driver=$(which chromedriver) -Dwebdriver.gecko.driver=$(which geckodriver) -jar $JAR -role node -hub http://$NETLOC:4444/grid/register/ -log log_.txt &
 # See https://stackoverflow.com/a/52829112/1913726
 # java -Dwebdriver.chrome.driver=/path/to/chromedriver.exe -jar /Users/admin/selenium-server-standalone-3.14.0.jar -role node -hub http://<IP_GRID_HUB>:4444/grid/register
