62c47d2caa40c6ab46531be6e6242b1108791aec
Don Morehouse <dm.wyncode@gmail.com>
2021-01-01 15:18:35 +0000

    Simplify rewrite. Not functioning. 	modified:   test/test_remote.py

--- test/test_remote.py
+++ test/test_remote.py
@@ -2,9 +2,6 @@ import os
 import shutil
 import sys
 from os import environ
-from random import choice, randint
-from string import ascii_lowercase
-from urllib.parse import urlunsplit
 
 import pytest
 import selenium.webdriver.common.desired_capabilities
@@ -35,7 +32,7 @@ class TestRemote(object):
         proxy.http_proxy = self.client.proxy
 
         self.driver = webdriver.Remote(
-            command_executor=COMMAND_EXECUTOR, options=options, proxy=proxy
+            command_executor=COMMAND_EXECUTOR, options=options
         )
 
     def teardown_method(self, method):
@@ -44,42 +41,25 @@ class TestRemote(object):
 
     @pytest.mark.human
     def test_set_clear_url_rewrite_rule(self):
-        """Assume HTTP server running on localhost with a document
-        named 000.html"""
-
-        query__fragment = ("",) * 2
-        tokens = scheme, netloc, path, query, fragment = (
-            "http",
-            "localhost:8000",
-            "000.html",
-            *query__fragment,
-        )
-        canonical_url = urlunsplit(tokens)
-        random_word = "".join((choice(ascii_lowercase) for _ in range(randint(1, 10))))
-        target_url = urlunsplit(
-            (scheme, netloc, f"{random_word}.html", *query__fragment)
-        )
-        needle = "64796c38a7f54d5ab735cdadd575a9ca"
-        self.driver.get(canonical_url)
-        assert needle in self.driver.page_source
-
+        targetURL = "http://localhost:8000/versions.js"
         assert 200 == self.client.rewrite_url(
-            r"[a-z]+\.html$",
-            path,
+            "http://localhost:8000/versions.+",
+            "https://localhost:8000/versions.json",
         )
-        self.driver.get(target_url)
+        self.driver.get(targetURL)
+        needle = "Makayla Thomas"
         assert needle in self.driver.page_source
         assert self.client.clear_all_rewrite_url_rules() == 200
-        self.driver.get(rewrite_url)
-        assert needle not in self.driver.page_source
+        self.driver.get(targetURL)
+        assert "needle" not in self.driver.page_source
 
     @pytest.mark.human
     def test_response_interceptor(self):
         content = "Response successfully intercepted"
-        target_url = "https://saucelabs.com/versions.json?hello"
+        targetURL = "https://saucelabs.com/versions.json?hello"
         self.client.response_interceptor(
             """if(messageInfo.getOriginalUrl().contains('?hello')){contents.setTextContents("%s");}"""
             % content
         )
-        self.driver.get(target_url)
+        self.driver.get(targetURL)
         assert content in self.driver.page_source
